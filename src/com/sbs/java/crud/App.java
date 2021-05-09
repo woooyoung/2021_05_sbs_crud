51d2748d2b6f2d15974fc0c7797d8c1a796faccf
woooyoung <axdsw121@gmail.com>
2021-05-09 15:48:38 +0900

    특정 게시물을 찾는 메서드 구현 & 관련 중복 제거

--- src/com/sbs/java/crud/App.java
+++ src/com/sbs/java/crud/App.java
@@ -21,8 +21,6 @@ public class App {
 
 		Scanner sc = new Scanner(System.in);
 
-		// List<Article> articles = new ArrayList<>(); // article 담을 배열
-
 		while (true) {
 			System.out.printf("명령어 ) ");
 			String command = sc.nextLine().trim();
@@ -66,16 +64,7 @@ public class App {
 				String[] commandBits = command.split(" ");
 				int id = Integer.parseInt(commandBits[2]);
 
-				Article foundArticle = null;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-
-					if (article.id == id) {
-						foundArticle = article;
-						break;
-					}
-				}
+				Article foundArticle = getArticleById(id);
 
 				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
@@ -94,16 +83,7 @@ public class App {
 				String[] commandBits = command.split(" ");
 				int id = Integer.parseInt(commandBits[2]);
 
-				Article foundArticle = null;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-
-					if (article.id == id) {
-						foundArticle = article;
-						break;
-					}
-				}
+				Article foundArticle = getArticleById(id);
 
 				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
@@ -124,16 +104,7 @@ public class App {
 				String[] commandBits = command.split(" ");
 				int id = Integer.parseInt(commandBits[2]);
 
-				int foundIndex = -1;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-
-					if (article.id == id) {
-						foundIndex = i;
-						break;
-					}
-				}
+				int foundIndex = getArticleIndexById(id);
 
 				if (foundIndex == -1) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
@@ -153,6 +124,57 @@ public class App {
 
 		System.out.println("==프로그램 끝==");
 	}
+
+	private int getArticleIndexById(int id) {
+		int i = 0;
+		for (Article article : articles) {
+			if (article.id == id) {
+				return i;
+			}
+			i++;
+		}
+		return -1;
+
+	}
+
+//	for (int i = 0; i < articles.size(); i++) {
+//		Article article = articles.get(i);
+//
+//		if (article.id == id) {
+//			foundIndex = i;
+//			break;
+//		}
+//	}
+	private Article getArticleById(int id) {
+		int index = getArticleIndexById(id);
+
+		if (index != -1) {
+			return articles.get(index);
+		}
+
+		return null;
+	}
+//	private Article getArticleById(int id) {
+//		for (int i = 0; i < articles.size(); i++) {
+//			Article article = articles.get(i);
+//
+//			if (article.id == id) {
+//				return article;
+//			}
+//
+//		}
+//
+//		return null;
+//	}
+
+//	for (int i = 0; i < articles.size(); i++) {
+//	Article article = articles.get(i);
+//
+//	if (article.id == id) {
+//		foundArticle = article;
+//		break;
+//	}
+//}
 
 	private static void makeTestData() {
 		System.out.println("테스트를 위한 데이터를 생성합니다.");
