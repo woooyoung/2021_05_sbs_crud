2edb1cfe67b0249386d66f1443d18f965a996c99
woooyoung <axdsw121@gmail.com>
2021-05-16 15:54:15 +0900

    로그인 상태 체크 & 로그아웃 구현

--- src/com/sbs/java/crud/controller/MemberController.java
+++ src/com/sbs/java/crud/controller/MemberController.java
@@ -32,13 +32,34 @@ public class MemberController extends Controller {
 		case "login":
 			doLogin();
 			break;
+		case "logout":
+			doLogout();
+			break;
 		default:
 			System.out.println("존재하지 않는 명령어 입니다.");
 			break;
 		}
 	}
 
+	private boolean isLogined() {
+		return loginedMember != null;
+	}
+
+	private void doLogout() {
+		if (isLogined() == false) {
+			System.out.println("로그인 상태가 아닙니다.");
+			return;
+		}
+
+		loginedMember = null;
+		System.out.println("로그아웃 되었습니다.");
+	}
+
 	private void doLogin() {
+		if (isLogined()) {
+			System.out.println("이미 로그인 되어있습니다.");
+			return;
+		}
 		System.out.printf("로그인 아이디 : ");
 		String loginId = sc.nextLine();
 		System.out.printf("로그인 비밀번호 : ");
@@ -135,6 +156,14 @@ public class MemberController extends Controller {
 		members.add(member);
 
 		System.out.printf("%d번 회원이 생성 되었습니다.\n", id);
+	}
+
+	public void makeTestData() {
+		System.out.println("테스트를 위한 회원 데이터를 생성합니다.");
+
+		members.add(new Member(1, Util.getNowDateStr(), "admin", "admin", "관리자"));
+		members.add(new Member(2, Util.getNowDateStr(), "user1", "user1", "유저1"));
+		members.add(new Member(3, Util.getNowDateStr(), "user2", "user2", "유저2"));
 	}
 
 }
